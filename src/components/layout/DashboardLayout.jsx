884bdebe53e0f79a5161b329a18b302b446b6d76
Archis009 <vedvadnere@gmail.com>
2026-01-30 02:07:10 +0530

    feat: add dashboard layout wrapper

---
+++ src/components/layout/DashboardLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+const DashboardLayout = () => {
+  return (
+    <div className="flex h-screen bg-light-bg dark:bg-dark-bg text-slate-900 dark:text-slate-100 overflow-hidden">
+      <Sidebar />
+      <div className="flex-1 flex flex-col h-screen overflow-hidden">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto p-6 scroll-smooth">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
