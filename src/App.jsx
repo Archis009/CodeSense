6387d29f40d22cd9a84c4b436dcf86c10577cfb6
Archis009 <vedvadnere@gmail.com>
2026-01-30 02:06:57 +0530

    feat: add imports for dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
+import DashboardLayout from './components/layout/DashboardLayout';
 
 function App() {
   return (
